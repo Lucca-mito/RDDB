18ac361b8ce29e47f3423a08dc616b90552b93c3
e-zheng <35248807+e-zheng@users.noreply.github.com>
2022-03-17 01:29:54 -0700

    4 queries done

--- queries.sql
+++ queries.sql
@@ -8,21 +8,33 @@ WITH spending AS (
 )
 SELECT uid, order_date AS date, total_spending AS max_spending 
 FROM (SELECT order_date, MAX(total_spending) AS total_spending FROM spending
-GROUP BY order_date) t NATURAL JOIN spending;
+GROUP BY order_date) t NATURAL JOIN spending ORDER BY order_date ASC;
 
--- Which hour was busiest and what percentage of total orders occurred in that 
--- hour? 
--- WITH hourly_orders AS (
---   SELECT HOUR(order_time) as hour_of_day, COUNT(order_number) AS num_orders 
---   FROM rd_order GROUP BY HOUR(order_time)
--- )
--- SELECT hour_of_day, MAX(order_number)
--- ;
+-- Which hour was busiest and how many orders? 
+WITH hourly_orders AS (
+  SELECT HOUR(order_time) as hour_of_day, COUNT(order_number) AS num_orders 
+  FROM rd_order GROUP BY HOUR(order_time)
+), most_orders AS (
+  SELECT MAX(num_orders) as num_orders FROM hourly_orders 
+)
+  SELECT hour_of_day, num_orders AS max_orders FROM 
+  (SELECT * FROM most_orders NATURAL JOIN hourly_orders) t
+;
 
+-- A student wants to get a list of all vegan options for ease 
+-- sorted by popularity (descending). 
+WITH vegan_items AS (
+  SELECT item_id, item_name FROM item 
+  WHERE vegetarian = TRUE AND dairy_free = TRUE
+)
+SELECT item_id, item_name, count(*) AS times_ordered 
+FROM order_item NATURAL JOIN vegan_items 
+WHERE item_id IN (SELECT item_id FROM vegan_items)
+GROUP BY item_id, item_name ORDER BY times_ordered DESC
+;
 
--- Compute the largest different in the remaining declining balance for all 
--- flex students. 
-
--- Student x is dropping out, remove them from the query. 
--- The price of beef has recently increased, update the value of beef and update
--- 
+-- Calculate the total remaining balance for all students on the flex plan
+-- Recall that students start with a balance of $525.00
+SELECT uid, first_name, last_name, 525 - SUM(order_total) AS remaining_balance 
+FROM rd_order NATURAL JOIN student 
+WHERE plan = 'flex' GROUP BY uid, first_name, last_name;
